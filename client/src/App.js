09e8ad8f28c85348c04630805bb59fc37968136b
Selvakumar <selvakumard274@gmail.com>
2024-02-13 22:24:19 +0530

    api level workstation

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from 'react'
 
 const App = () => {
-  const [data,setData]=useState('');
+  const [data,setData]=useState([]);
   useEffect(function fetchdata(){
-    fetch("http://localhost:3000").then(res=>res.text())
+    fetch("http://localhost:3000/users").then(res=>res.json())
     .then(res=>setData(res)).catch((err)=>console.log(err))
   },[]);
   return (
-    <div>
-      <h1>{data}</h1>
-    </div>
+    <>
+     {
+      data.map(i=>{
+         return <div >
+      <h1 key={i.id}>{i.name}</h1>
+      <p key={i.id}>{i.age}</p>
+      </div>
+      })
+     }
+    </>
   )
 }
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+const data=[
+  {name:"selvakumar",
+age:20},
+{name:"thirumalai",
+age:25},{
+  name:"Amos",
+  age:30
+}
+]
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(data);
 });
 
 module.exports = router;

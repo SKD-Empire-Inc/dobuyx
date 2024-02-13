79c4231ba095f83e91b931c7a51c6d0b3938c14a
Selvakumar <selvakumard274@gmail.com>
2024-02-13 22:05:49 +0530

    conections made bye selva

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+  const [data,setData]=useState('');
+  useEffect(function fetchdata(){
+    fetch("http://localhost:3000").then(res=>res.text())
+    .then(res=>setData(res)).catch((err)=>console.log(err))
+  },[]);
   return (
-    <div>App</div>
+    <div>
+      <h1>{data}</h1>
+    </div>
   )
 }
 
